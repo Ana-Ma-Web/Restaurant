e1175bbbeb786ccef8d0ce04378f9eda81c26642
Ana-Ma-Web <ana.web@mail.ru>
2021-09-14 16:01:34 +0300

    202109141601

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ let Swiper2 = new Swiper(sliderGallery, {
 // ============ HEADER BG ==============
 function scrollHeader() {
    const header = document.getElementById('header')
-   if (this.scrollY >= 50) header.classList.add ('scroll-header'); else header.classList.remove ('scroll-header')
+   if (this.scrollY >= 300) header.classList.add ('scroll-header'); else header.classList.remove ('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
